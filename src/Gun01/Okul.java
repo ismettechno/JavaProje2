46db6f880d6f8c6ad7c68b9f5f21eca6e60d6041
TechnoStudy <ismettechno@gmail.com>
2025-03-03 16:16:44 +0000

    Okul main yazıldı

---
+++ src/Gun01/Okul.java
@@ -0,0 +1,9 @@
+package Gun01;
+
+public class Okul {
+    public static void main(String[] args) {
+
+        Ogrenci ogr1=new Ogrenci("İsmet","Temur",1);
+        System.out.println(ogr1);
+    }
+}
