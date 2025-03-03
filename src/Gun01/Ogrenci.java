4b57a9cb1bdd97b10ee08919f2dd3c6812712901
TechnoStudy <ismettechno@gmail.com>
2025-03-03 16:12:19 +0000

    Öğrenci class ı tamamlandı

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -5,5 +5,42 @@ public class Ogrenci {
     private String ad;
     private String soyad;
 
+    public Ogrenci(String ad, String soyad, int okulNo) {
+        this.ad = ad;
+        this.soyad = soyad;
+        this.okulNo = okulNo;
+    }
 
+    public int getOkulNo() {
+        return okulNo;
+    }
+
+    public void setOkulNo(int okulNo) {
+        this.okulNo = okulNo;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    @Override
+    public String toString() {
+        return "Ogrenci{" +
+                "okulNo=" + okulNo +
+                ", ad='" + ad + '\'' +
+                ", soyad='" + soyad + '\'' +
+                '}';
+    }
 }
